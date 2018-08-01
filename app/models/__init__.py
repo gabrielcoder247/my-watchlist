8b9d3eb0b934f6227dcc8c508ac981c51b7ae0e3
gabrielcoder <gabrielcoder247@gmail.com>
2018-08-01 19:02:05 +0300

    function to process api response

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -12,6 +12,7 @@ class MovieTest(unittest.TestCase):
     '''
     set up method that will run before every Test
     '''
+    self.new_movie = Movie(1234,'python Must Be Crazy', 'A thrilling new python Series','https://image.tmdb.org/t/p/w500/khsjha27hbs',8.5,129993)
     def test_instance(self):
         self.assertTrue(isinstance(self.new_movie,Movie))
 
