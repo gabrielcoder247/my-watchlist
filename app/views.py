5ef07ac47c9bb6ec64d8f42052a6614d9bb717de
gabrielcoder <gabrielcoder247@gmail.com>
2018-08-02 12:52:56 +0300

    add movies categories in the views and index html templates

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_movies
 
 #Views
 
@@ -8,8 +9,14 @@ def index():
     '''
     View root page function that returns the index page and it's data
     '''
-    message = 'Hello World, we are here to help you'
-    return render_template('index.html', message = message)
+    #Getting popular movie
+    popular_movies = get_movies('popular')
+    upcoming_movies = get_movies('upcoming')
+    now_showing_movies = get_movies('now_playing')
+    print(popular_movies)
+    title = 'Home - Welcome to the Movie Review Website Online'
+  
+    return render_template('index.html', title = title,popular = popular_movies, upcoming = upcoming_movies, now_showing = now_showing_movies)
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
