6eb2e506bb1b243c1d8fdc3b54e8ba5dd6eb249f
gabrielcoder <gabrielcoder247@gmail.com>
2018-08-05 13:24:48 +0300

    add review form

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from flask import render_template,request,redirect,url_for
 from app import app
-from .request import get_movies,search_movie
+from .request import get_movies,get_movie,search_movie
+from .models import reviews
+from .forms import ReviewForm
+Review = reviews.Review
 
 #Views
 
@@ -27,7 +30,7 @@ def index():
         return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movies, now_showing = now_showing_movies)
 
 @app.route('/movie/<int:id>')
-def movie(movie_id):
+def movie(id):
 
     '''
     View movie page function that returns the movie details page and its data
@@ -35,7 +38,7 @@ def movie(movie_id):
 
     movie = get_movie(id)
     title= f'{movie.title}'
-    return render_template('movie.html', title=title, movie = movie_id)
+    return render_template('movie.html', title=title, movie = movie)
 
 @app.route('/search/<movie_name>')
 def search(movie_name):
@@ -48,3 +51,19 @@ def search(movie_name):
     title = f'search results for {movie_name}'
     
     return render_template('search.html', movies = searched_movies)
+
+@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+def new_Review(id):
+    form = ReviewForm()
+    movie = get_movie(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+        new_review = Review(movie.id, title,movie.poster,review)
+        new_review.save_review()
+        return redirect(url_for('movie',id = movie.id))
+
+    title = f'{movie.title} review'
+
+    return render_template('new_review.html',title = title, review_form = form, movie = movie )
